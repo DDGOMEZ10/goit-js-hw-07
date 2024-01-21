4f7a7c2002cd4b84dbb7a478b6d1e9525dbefee1
DDGOMEZ10 <123340041+DDGOMEZ10@users.noreply.github.com>
2024-01-21 13:45:55 -0500

    corregido

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,7 +2,11 @@
 
 import { galleryItems } from './gallery-items.js';
 
+console.log(galleryItems);
+
 const galleryContainer = document.querySelector(".gallery");
+
+console.log(galleryContainer);
 
 function createGallery(galleryItems) {
   return galleryItems
@@ -11,6 +15,7 @@ function createGallery(galleryItems) {
         `<li class="gallery__item">
           <a class="gallery__link" href="${original}">
             <img
+            
               class="gallery__image"
               src="${preview}"
               data-source="${original}"
@@ -28,14 +33,11 @@ galleryContainer.addEventListener("click", openModal);
 
 function openModal(e) {
   e.preventDefault();
-  const image = e.target.querySelector('.gallery__image');
-  const src = image.dataset.source;
-  const alt = image.alt;
-  lightbox.content.innerHTML = `<img src="${src}" alt="${alt}">`;
-  lightbox.open();
 }
 
-var lightbox = new SimpleLightbox('.gallery a', {
-  captionData: "alt",
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
   captionDelay: "250ms",
 });
+
+
